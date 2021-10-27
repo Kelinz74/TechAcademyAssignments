c2bb329446cefa9a6e92477e5237f7d52ecc5e4c
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-10-27 01:01:15 -0700

    Visual Studio HTML Submission Assignment

---
+++ Favorite Music Video/FavoriteMusicVideo/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 700) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + "px";
+            elem.style.right = pos + "px";
+        }
+    }
+
+}
